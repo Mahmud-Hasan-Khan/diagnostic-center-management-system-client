3c9d46733dc601934fc6c452a73e09fcfd025d03
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-12-03 12:42:55 +0600

    Reservation page done

--- src/pages/Dashboard/Admin/Reservation.jsx
+++ src/pages/Dashboard/Admin/Reservation.jsx
@@ -5,11 +5,14 @@ import SectionTitle from "../../../components/shared/SectionTitle/SectionTitle";
 import moment from "moment";
 import { FaSearch } from "react-icons/fa";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 const Reservation = () => {
 
     const axiosSecure = useAxiosSecure();
     const [searchQuery, setSearchQuery] = useState('');
+    const [reportLinks, setReportLinks] = useState("");
+
     //data Load Using TanStack Query
     const { data: reservations = [], refetch, isPending: loading } = useQuery({
         queryKey: ['reservations', searchQuery],
@@ -22,13 +25,66 @@ const Reservation = () => {
         }
     });
 
+    const handleCancelReservation = (id) => {
+        Swal.fire({
+            title: "Are you sure to cancel the Reservation?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, I want it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/upcomingAppointment/${id}`)
+                    .then(res => {
+                        console.log(res.data)
+                        if (res.data.modifiedCount > 0) {
+                            Swal.fire({
+                                title: "Updated!",
+                                text: "Reservation has been canceled",
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+            }
+        });
+    }
+    const handleReportStatus = (id) => {
+        Swal.fire({
+            title: "Are you sure to Upload the Test Result?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, I want it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/updateReportStatus/${id}`, { reportLink: reportLinks[id] })
+                    .then(res => {
+                        console.log(res.data)
+                        if (res.data.modifiedCount > 0 || res.data.upsertedCount > 0) {
+                            Swal.fire({
+                                title: "Updated!",
+                                text: "Test Result has been uploaded",
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+            }
+        });
+    }
+
     return (
         <div className="bg-white rounded-lg overflow-hidden">
             <Helmet>
                 <title>Reservations | Admin Dashboard</title>
             </Helmet>
             <div >
-                <SectionTitle heading="All The Reservations " subheading="Specific test Appointment "></SectionTitle>
+                <SectionTitle heading="All The Reservations " subheading="Update Reservation Status"></SectionTitle>
                 <div>
                     <div className="flex flex-col-reverse items-center justify-between">
                         <div className="mx-auto pb-4">
@@ -56,7 +112,7 @@ const Reservation = () => {
                         </div>
                     ) : reservations && reservations.length > 0 ? (
                         <div className="px-6">
-                            <h4 className="text-center text-lg text-[#ac52b4] font-medium pb-2">Total Test Appointment Found {reservations.length}</h4>
+                            <h4 className="text-center text-lg text-[#ac52b4] font-medium pb-2">Total Reservation Found {reservations.length}</h4>
                             <div className="overflow-x-auto">
                                 <table className="table">
                                     {/* head */}
@@ -81,19 +137,46 @@ const Reservation = () => {
                                                     <p className="bg-base-300 rounded-full px-2 text-center text-base">{test?.email}</p>
                                                 </td>
                                                 <td>{moment(test?.appointmentDate).format("Do MMM YYYY")}</td>
-                                                <td>{test?.testStatus}</td>
-                                                <td>{test?.reportStatus}</td>
-                                                {/* <td>
-                                                    {test?.testStatus === 'Canceled' ? <button disabled onClick={() => handleCancelAppointment(test._id)}
-                                                        className="bg-base-300  p-2 rounded text-[#e00000] font-medium"
+                                                <td>
+                                                    {test?.testStatus === 'Canceled' ? (<span className="bg-red-100  px-4 py-2 rounded text-[#e00000] font-medium"
                                                     >
-                                                        Already Canceled
-                                                    </button> : <button onClick={() => handleCancelAppointment(test._id)}
+                                                        Canceled
+                                                    </span>) : test?.testStatus === 'Done' ? (<span className="bg-green-300 px-4 py-2 rounded font-medium" > Test Done </span>) : <button onClick={() => handleCancelReservation(test._id)}
                                                         className="bg-[#e00000] hover:bg-[#ff9416] p-2 rounded text-white font-medium"
                                                     >
-                                                        Cancel Appointment
+                                                        Cancel Now
                                                     </button>}
-                                                </td> */}
+                                                </td>
+
+                                                <td>
+                                                    {test?.reportStatus === 'Delivered' ? (
+                                                        <button disabled className="bg-[#00afef24] px-4 py-2 rounded text-[#00AEEF] font-medium">
+                                                            Delivered
+                                                        </button>
+                                                    ) : test?.testStatus === 'Canceled' ? (
+                                                        <span className="bg-red-100  px-4 py-2 rounded text-[#e00000] font-medium">Reservation Canceled</span>
+                                                    ) : (
+                                                        <div className="flex gap-1">
+                                                            <div>
+                                                                <input
+                                                                    type="url"
+                                                                    placeholder="Enter Report Link"
+                                                                    required
+                                                                    value={reportLinks[test._id] || ''}
+                                                                    onChange={(e) => setReportLinks({ ...reportLinks, [test._id]: e.target.value })}
+                                                                    className="px-3 py-2 border rounded-md border-gray-300 focus:outline-[#00d260] bg-base-200 text-gray-900 w-36"
+                                                                />
+                                                            </div>
+                                                            <button
+                                                                onClick={() => handleReportStatus(test._id)}
+                                                                className="bg-lime-500 hover:bg-[#ff9416] p-2 rounded text-white font-medium"
+                                                            >
+                                                                Submit Report
+                                                            </button>
+                                                        </div>
+                                                    )}
+                                                </td>
+
                                             </tr>
                                         ))}
                                     </tbody>
